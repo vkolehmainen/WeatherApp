06349cf7f71146ed0d79c1dfecce4d53be05415f
Ville Kolehmainen <kolehmv2@aalto.fi>
2018-01-09 19:18:17 +0200

    add weather type database model

--- main/models.py
+++ main/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 
 # Create your models here.
+TYPES = (
+    ('SUN', 'Sunny'),
+    ('RAIN', 'Rainy'),
+    ('CLOUD', 'Cloudy'),
+    ('SNOW', 'Snowy'),
+    ('STORM', 'Stormy')
+)
 
 class City(models.Model):
     name = models.CharField(max_length=255, unique = True)
     x_coord = models.FloatField()
     y_coord = models.FloatField()
 
+    def __str__(self):
+       return self.name
+
 class Observation(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     temperature = models.FloatField()
     time = models.TimeField()
+
+    weather_type = models.CharField(
+        max_length = 5,
+        choices=TYPES,
+        default="SUN",
+    )
